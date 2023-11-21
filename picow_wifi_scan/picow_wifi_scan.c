9090e5d9d2be95ab33575a538f8f307c66079ce6
Chris <cc2698@cornell.edu>
2023-11-21 14:13:23 -0500

    Store last pico found during scan

--- picow_wifi_scan/picow_wifi_scan.c
+++ picow_wifi_scan/picow_wifi_scan.c
@@ -19,11 +19,13 @@
 #include "hardware/clocks.h"
 #include "hardware/vreg.h"
 
+char last_pico_found[100];
+
 // Wifi scan callback function, prints cyw43_ev_scan_result_t as a string
-static int scan_result(void* env, const cyw43_ev_scan_result_t* result)
+static int print_result(void* env, const cyw43_ev_scan_result_t* result)
 {
     if (result) {
-        char header[10];
+        char header[10] = "";
 
         // Get first 5 characters of the SSID
         *header = '\0';
@@ -39,6 +41,33 @@ static int scan_result(void* env, const cyw43_ev_scan_result_t* result)
             printf("ssid: %-32s rssi: %4d chan: %3d mac: %s sec: %u\n",
                    result->ssid, result->rssi, result->channel, bssid_str,
                    result->auth_mode);
+        }
+    }
+
+    return 0;
+}
+
+static int store_result(void* env, const cyw43_ev_scan_result_t* result)
+{
+    if (result) {
+        char header[10] = "";
+
+        // Get first 5 characters of the SSID
+        *header = '\0';
+        snprintf(header, 6, "%s", result->ssid);
+
+        // Compose MAC address
+        char bssid_str[40];
+        sprintf(bssid_str, "%02x:%02x:%02x:%02x:%02x:%02x", result->bssid[0],
+                result->bssid[1], result->bssid[2], result->bssid[3],
+                result->bssid[4], result->bssid[5]);
+
+        printf("ssid: %-32s rssi: %4d chan: %3d mac: %s sec: %u\n",
+               result->ssid, result->rssi, result->channel, bssid_str,
+               result->auth_mode);
+
+        if (strcmp(header, "picow") == 0) {
+            snprintf(last_pico_found, 100, "%s", result->ssid);
         }
     }
 
@@ -82,7 +111,9 @@ int main()
                 // This function scans for nearby Wifi networks and runs the
                 // callback function each time a network is found.
                 int err = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL,
-                                          scan_result);
+                                          store_result);
+
+                printf("\n\n\nlast_pico_found = %s\n\n\n", last_pico_found);
 
                 if (err == 0) {
                     printf("\nPerforming wifi scan\n");
