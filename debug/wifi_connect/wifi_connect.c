82657705e5cce71f152718d545bd87fb5d2c5127
cc2698 <cc2698@cornell.edu>
2023-11-24 19:12:22 -0500

    Moved debugging demos (blink, scan, access point, connect) into a separate directory

---
+++ debug/wifi_connect/wifi_connect.c
@@ -0,0 +1,44 @@
+
+// C Libraries
+#include <stdlib.h>
+#include <string.h>
+
+// Pico
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
+// Wifi name and password
+#define WIFI_SSID     "picow_1"
+#define WIFI_PASSWORD "password"
+
+int main()
+{
+    stdio_init_all();
+
+    printf("\n\n==================== %s ====================\n\n",
+           "Wifi Scanner");
+
+    // Initialize Wifi chip
+    printf("Initializing cyw43...");
+    if (cyw43_arch_init()) {
+        printf("failed to initialise.\n");
+        return 1;
+    } else {
+        printf("initialized!\n");
+    }
+
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD,
+                                           CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        return 1;
+    } else {
+        printf("Connected.\n");
+    }
+
+    cyw43_arch_deinit();
+
+    return 0;
+}
--- debug/wifi_scan/picow_wifi_scan.c
+++ debug/wifi_scan/picow_wifi_scan.c
@@ -25,37 +25,6 @@ char last_pico_found[100];
 static int print_result(void* env, const cyw43_ev_scan_result_t* result)
 {
     if (result) {
-        char header[10] = "";
-
-        // Get first 5 characters of the SSID
-        *header = '\0';
-        strncat(header, result->ssid, 5);
-
-        if (strcmp(header, "picow") == 0) {
-            // Compose MAC address
-            char bssid_str[40];
-            sprintf(bssid_str, "%02x:%02x:%02x:%02x:%02x:%02x",
-                    result->bssid[0], result->bssid[1], result->bssid[2],
-                    result->bssid[3], result->bssid[4], result->bssid[5]);
-
-            printf("ssid: %-32s rssi: %4d chan: %3d mac: %s sec: %u\n",
-                   result->ssid, result->rssi, result->channel, bssid_str,
-                   result->auth_mode);
-        }
-    }
-
-    return 0;
-}
-
-static int store_result(void* env, const cyw43_ev_scan_result_t* result)
-{
-    if (result) {
-        char header[10] = "";
-
-        // Get first 5 characters of the SSID
-        *header = '\0';
-        snprintf(header, 6, "%s", result->ssid);
-
         // Compose MAC address
         char bssid_str[40];
         sprintf(bssid_str, "%02x:%02x:%02x:%02x:%02x:%02x", result->bssid[0],
@@ -65,10 +34,6 @@ static int store_result(void* env, const cyw43_ev_scan_result_t* result)
         printf("ssid: %-32s rssi: %4d chan: %3d mac: %s sec: %u\n",
                result->ssid, result->rssi, result->channel, bssid_str,
                result->auth_mode);
-
-        if (strcmp(header, "picow") == 0) {
-            snprintf(last_pico_found, 100, "%s", result->ssid);
-        }
     }
 
     return 0;
@@ -79,7 +44,6 @@ int main()
     // Initialize all stdio types
     stdio_init_all();
 
-    // Print header
     printf("\n\n==================== %s ====================\n\n",
            "Wifi Scanner");
 
@@ -111,9 +75,7 @@ int main()
                 // This function scans for nearby Wifi networks and runs the
                 // callback function each time a network is found.
                 int err = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL,
-                                          store_result);
-
-                printf("\n\n\nlast_pico_found = %s\n\n\n", last_pico_found);
+                                          print_result);
 
                 if (err == 0) {
                     printf("\nPerforming wifi scan\n");
@@ -133,5 +95,6 @@ int main()
     }
 
     cyw43_arch_deinit();
+
     return 0;
 }
